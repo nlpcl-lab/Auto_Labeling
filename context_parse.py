1756dc3957f23cc9675667d569b282d5e95bfe93
zomss <nelllpic@kaist.ac.kr>
2022-01-03 22:15:29 +0900

    Merge remote-tracking branch 'Auto_Labeling/master'

--- context_parse.py
+++ context_parse.py
@@ -36,7 +36,7 @@ if __name__=="__main__":
         configs = DictObj(configs)
 
     dataset = configs.input.dataset
-    save_path = configs.input.save_path.format(dataset)
+    save_path = configs.input.save_path
     rand = configs.input.random
     logging.basicConfig(format='%(asctime)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S',
@@ -51,41 +51,47 @@ if __name__=="__main__":
     data_path = os.path.join(out_dir, dataset)
 
     corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="train")
-
-    with open(save_path,'w') as f:
-        for k,v in tqdm(qrels.items()):
-            v = list(v.keys())[0]
-            query = queries[k]
-            context = corpus[v]
-            doc = nlp(context['text'])
-            if len(doc.ents) != 0:
-                ents = list(doc.ents)
-                if rand == "True":
-                    print('check')
-                    random.shuff(ents)
-                sub_type = wiki.get_categories(context['title'])
-                for ent in ents:
-                    if ent.label_ == "DATE" or ent.label_ == 'TIME' or ent.label_ == 'CARDINAL':
-                        obj_type = ""
+    result = []
+    index = 0
+    for k,v in tqdm(qrels.items()):
+        v = list(v.keys())[0]
+        query = queries[k]
+        context = corpus[v]
+        doc = nlp(context['text'])
+        if len(doc.ents) != 0:
+            ents = list(doc.ents)
+            if rand == "True":
+                print('check')
+                random.shuff(ents)
+            sub_type = wiki.get_categories(context['title'])
+            for ent in ents:
+                if ent.label_ == "DATE" or ent.label_ == 'TIME' or ent.label_ == 'CARDINAL':
+                    obj_type = ""
+                    obj_sum = [str(s) for s in list(doc.sents) if ent.text in str(s)][0]
+                else:
+                    obj_type = wiki.get_categories(ent.text)
+                    obj_sum = wiki.get_summary(ent.text)
+                    if obj_sum != "":
+                        obj_sum = nlp(obj_sum)
+                        obj_sum = list(obj_sum.sents)[0]
+                    if str(obj_sum) == "" or "\n" in str(obj_sum):
                         obj_sum = [str(s) for s in list(doc.sents) if ent.text in str(s)][0]
-                    else:
-                        obj_type = wiki.get_categories(ent.text)
-                        obj_sum = wiki.get_summary(ent.text)
-                        if obj_sum != "":
-                            obj_sum = nlp(obj_sum)
-                            obj_sum = list(obj_sum.sents)[0]
-                        if str(obj_sum) == "" or "\n" in str(obj_sum):
-                            obj_sum = [str(s) for s in list(doc.sents) if ent.text in str(s)][0]
-                    sub = {'name' : context['title'],
-                           'type' : sub_type,
-                           'des' : context['text']}
-                    pre = {'name' : ent.label_.lower(),
-                           'token' : ""}
-                    obj = {'name' : ent.text,
-                           "type" : obj_type,
-                           "des" : obj_sum
-                           }
-                    text = make_text(sub,pre,obj,v)
-                    f.write(text)
-
+                sub = {'name' : context['title'],
+                       'type' : sub_type,
+                       'des' : context['text']}
+                pre = {'name' : ent.label_.lower(),
+                       'token' : ""}
+                obj = {'name' : ent.text,
+                       "type" : obj_type,
+                       "des" : obj_sum
+                       }
+                text = make_text(sub,pre,obj,v)
+                result.append(text)
+                if len(result) >= 1000:
+                    print("Save_Data")
+                    with open(save_path.format(dataset,index), 'w') as f:
+                        for text in result:
+                            f.write(text)
+                        index = index + 1
+                        result = []
 
